2aef2bb98847a337994dd2a7de5428cd012c49c9
Daniel Bali <daniel.bali@agcocorp.com>
2023-10-17 14:20:36 +0200

    update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Example } from '@shummy1991/package-test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Example />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
